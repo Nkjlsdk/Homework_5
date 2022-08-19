6d6662e4e327a09cc90ac4a13d16b9d9691e6164
Your Name <e@w.com>
2022-08-19 15:36:48 +0300

    ввод одномерного массива, вывод суммы элементов на нечетных позициях

---
+++ Task_36/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ввести одномерный массив случайных чисел, найти сумму элементов стоящих на нечетных позициях
+int [] arrayrandom = new int [4];
+int sum = 0;
+for (int i = 0; i < arrayrandom.Length; i++)
+{
+    arrayrandom[i] = new Random().Next(-100,100);
+    
+    if (i%2 != 0) sum += arrayrandom[i];
+    
+}
+Console.WriteLine($"случайный массив: [{String.Join("; ", arrayrandom)}]");
+Console.WriteLine($"Сумма элементов на нечетных позициях: {sum}");
