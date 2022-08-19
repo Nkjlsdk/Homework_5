4570894bd4f6e824e356eb46a54c1d4c033dcfcc
Your Name <e@w.com>
2022-08-19 15:29:56 +0300

    ввод случайного массива, вывод количества четных чисел в нем

---
+++ Task_34/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ввести массив случайных положительных трехзначных чисел, показать количестов четных чисел
+int [] arrayrandom = new int [4];
+int sum = 0;
+
+for (int i = 0; i <arrayrandom.Length; i++)
+{
+    arrayrandom[i] = new Random().Next(100,1000);
+       if (arrayrandom[i]%2 == 0) sum++;
+}
+
+Console.WriteLine($"случайный массив: [{String.Join("; ", arrayrandom)}]");
+Console.WriteLine($"Количество четных элементов: {sum}");
+  
